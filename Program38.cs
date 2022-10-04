da1be39bb9c5b5fe0b2a8ec25142aff57a5b297b
422702 <114009839+422702@users.noreply.github.com>
2022-10-05 01:52:03 +0700

    Дз

---
+++ Program38.cs
@@ -0,0 +1,27 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+Console.WriteLine($"Введите массив вещественных чисел:");
+double[] array=CreateArray();
+Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {CalculatingDifference(array)}");
+
+
+double[] CreateArray()
+{
+    string[] text = Console.ReadLine().Split(" ");
+    double[] array = new double[text.Length];
+    for (int i = 0; i < text.Length; i++)
+    {
+         array[i] = double.Parse(text[i]);
+    }
+    return array;
+}
+
+double CalculatingDifference(double[] array)
+{
+    double max=array[0],min=array[0];
+    for(int i=1;i<array.Length;i++)
+    {
+        if(max<array[i]) max=array[i];
+        if(min>array[i]) min=array[i];
+    }
+ return max-min;
+}
